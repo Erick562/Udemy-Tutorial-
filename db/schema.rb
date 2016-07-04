597eba9ee476c4717e6b92ddd6a0c98a07659dce
Erick Gracian <gracianerick@yahoo.com>
2016-07-04 02:01:49 -0700

    sign up users from UI

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160630202104) do
+ActiveRecord::Schema.define(version: 20160702050743) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20160630202104) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
