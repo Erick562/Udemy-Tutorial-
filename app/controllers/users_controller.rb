967330591149e1456ac8739445ef12fb4f8a7640
Erick Gracian <gracianerick@yahoo.com>
2016-07-20 15:18:36 -0700

    Add users listing and add article view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,25 +4,39 @@ class UsersController < ApplicationController
   	@user =User.new 
   end 
 
+  def index 
+    @users = User.all
+  end
+
   def create 
   	@user = User.new(user_params)
   	if @user.save 
   		flash[:success] = "welcome to the alpha blog #{@user.username}"
-  		redirect_to user_path #i added the userpath whicch was article_path 
-  	else 
+  		redirect_to article_path 
+    else
   		render 'new'
   	end 
   end
 
 
-
-  def show 
-    
-  end
-
   def edit 
   	@user=User.find(params[:id])
   end 
+
+
+  def update 
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = " Your account was successfully updated"
+      redirect_to articles_path 
+    else 
+      render 'edit'
+    end 
+  end 
+
+  def show 
+    @user = User.find(params[:id])
+  end
 
   private 
   def user_params 
--- db/migrate/20160618083940_create_articles.rb
+++ db/migrate/20160618083940_create_articles.rb
@@ -2,8 +2,11 @@ class CreateArticles < ActiveRecord::Migration
   def change
     create_table :articles do |t|
     	t.string :title
-    	
+    	t.text :description 
+
+    	t.timestamps
     	
     end 
   end
 end
+#added the description and time stamps 
